decc2d206018ddb2e152a3beef895efc66971d69
DebexeL <kallekala20@gmail.com>
2020-03-25 20:01:13 +0200

    GeoLocation data will not used until further notice. I don't want to deal with GDPR in any way just yet.

--- index.php
+++ index.php
@@ -9,12 +9,17 @@
 <body>  
 
 <div id="content1" class="content"><?php
+/*
+//GeoLocation data will not used until further notice. I don't want to deal with GDPR in any way just yet.
 //Include geolocation class
-require_once('geoplugin.class.php');
+//require_once('geoplugin.class.php');
 $geoplugin = new geoPlugin();
 //IP Geolocation check
 $geoplugin->locate();
 $geoloc = "{$geoplugin->countryCode}";
+//
+//
+*/
 
 /*
 Translations for the disclaimer. You are free to add translations for other languages you feel the need for.
@@ -55,6 +60,8 @@ This tool has two main purposes:
 	<div>
 		</br>
 	<?php
+	/*
+	//GeoLocation data will not used until further notice. I don't want to deal with GDPR in any way just yet.
 	//If your country code is "FI", print the disclaimer in finnish. For anything else, print it in english.
 		if ($geoloc == "FI")
 		{
@@ -64,13 +71,24 @@ This tool has two main purposes:
 		{
 			echo $ccEN;
 		}
+	//
+	//
+	*/
+	
+	//Just print the disclaimer in english.
+	echo $ccEN;
 	?>
 </div>
 <?php
+/*
+//GeoLocation data will not used until further notice. I don't want to deal with GDPR in any way just yet.
 //Kinda obvious, but this prints your current country code on the page for you to see.
 echo "Country Code: {$geoplugin->countryCode} <br />";
+//
+//
+*/
 ?>
 <hr />
-<a href="https://www.geoplugin.com/geolocation/" target="_new">IP Geolocation</a> by <a href="https://www.geoplugin.com/" target="_new">geoPlugin</a>
+<!-- <a href="https://www.geoplugin.com/geolocation/" target="_new">IP Geolocation</a> by <a href="https://www.geoplugin.com/" target="_new">geoPlugin</a> -->
 </body>
 </html>
